953839fb9a6a08281590b8e24cc3ccee87976b11
Lilly Hewitt <70710764+lillyhewitt@users.noreply.github.com>
2024-07-09 16:17:37 -0400

    Add files via upload

---
+++ Validations/DateValidation.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using System.Globalization;
+using WebApplication1.Interface;
+
+namespace WebApplication1.Validations
+{
+    public class DateValidation : IDateValidations
+    {
+        // returns true if date is within a month limit
+        public bool IsDateWithinMonths(DateTime date, int months)
+        {
+            // find dates six months before and after current date
+            DateTime monthsAfterCurrentDate = DateTime.Now.AddMonths(months);
+
+            // return false if date is not within month limit, return true if within limit
+            if (date > monthsAfterCurrentDate)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        // returns true if date is within a year limit
+        public bool IsDateWithinYears(DateTime startDate, int years)
+        {
+            // find date two years from current date
+            DateTime yearsAfterCurrentDate = DateTime.Now.AddYears(years);
+
+            // return false if date is not within year limit, return true if within limit
+            if (startDate > yearsAfterCurrentDate)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Validations/TimeValidation.cs
@@ -0,0 +1,18 @@
+﻿using WebApplication1.Interface;
+
+namespace WebApplication1.Validations
+{
+    public class TimeValidation : ITimeValidations
+    {
+        // check if hour is between 1-24
+        public bool IsValid(int hour)
+        {
+            // return false if hour is not between 1-24
+            if (hour < 1 || hour > 24)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
